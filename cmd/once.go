bc74473047e327ca29377a86738f698b9db693c1
Ihor Tomilenko <ihor@tomilenko.tk>
2017-06-30 19:20:39 -0600

    New command `once` (closes #6)

---
+++ cmd/once.go
@@ -0,0 +1,69 @@
+package cmd
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/sourcegraph/checkup"
+	"github.com/spf13/cobra"
+)
+
+var onceCmd = &cobra.Command{
+	Use:   "once",
+	Short: "Run checks once and exit",
+	Long: `The once subcommand runs checkups once and exits.
+
+The result of each check is saved to storage if configured.
+Additionally, if a notifier is configured, it will be
+called to analyze and potentially notify you of any
+problems.
+
+Either storage or notifier (or both) have to be configured.
+If both are missing the app will terminate.
+
+Example:
+
+  $ checkup once`,
+	Run: func(cmd *cobra.Command, args []string) {
+		var err error
+		var results []checkup.Result
+		notify = true
+
+		if len(args) != 0 {
+			fmt.Println(cmd.Long)
+			os.Exit(1)
+		}
+
+		c := loadCheckup()
+		if len(c.Checkers) == 0 {
+			log.Fatal("no checkers configured")
+		}
+		if c.Storage != nil {
+			err = c.CheckAndStore()
+		} else if c.Notifier == nil {
+			log.Fatal("neither storage nor notifier configured")
+		} else {
+			results, err = c.Check()
+		}
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		allHealthy := true
+		for _, result := range results {
+			if !result.Healthy {
+				allHealthy = false
+			}
+		}
+
+		if !allHealthy {
+			os.Exit(1)
+		}
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(onceCmd)
+}
